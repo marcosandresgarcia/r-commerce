ac9348b944a317bac481ea135524ff6290c63097
Marcos Garcia <markosandres45@hotmail.com>
2024-02-10 11:12:52 -0500

    add html base a css from electro-master

--- rcommerce/src/App.js
+++ rcommerce/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import './css/bootstrap.min.css'
+import './css/font-awesome.min.css'
+import './css/nouislider.min.css'
+import './css/slick.css'
+import './css/slick-theme.css'
+import './css/style.css'
+import Header from "./components/Base/Header";
+import Footer from "./components/Base/Footer";
+import Navigation from "./components/Base/Navigation";
+import Collections from "./components/Base/Collections";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <Navigation/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
---
+++ rcommerce/src/components/Base/Collections.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+function Collections() {
+    return (
+        <div class="section">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4 col-xs-6">
+						<div class="shop">
+							<div class="shop-img">
+                                <img src="#" alt=""></img>
+							</div>
+							<div class="shop-body">
+								<h3>Laptop  Collection</h3>
+								<a href="#" class="cta-btn">Shop now <i class="fa fa-arrow-circle-right"></i></a>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-4 col-xs-6">
+						<div class="shop">
+							<div class="shop-img">
+								<img src="#" alt=""></img>
+							</div>
+							<div class="shop-body">
+								<h3>Accessories Collection</h3>
+								<a href="#" class="cta-btn">Shop now <i class="fa fa-arrow-circle-right"></i></a>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-4 col-xs-6">
+						<div class="shop">
+							<div class="shop-img">
+								<img src="#" alt=""></img>
+							</div>
+							<div class="shop-body">
+								<h3>Cameras Collection</h3>
+								<a href="#" class="cta-btn">Shop now <i class="fa fa-arrow-circle-right"></i></a>
+							</div>
+						</div>
+					</div>
+
+				</div>
+
+			</div>
+		</div>
+    );
+}
+
+export default Collections
